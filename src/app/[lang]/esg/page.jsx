4841ae5c5d1b46df05d987f07b6af2bff1cd72b5
Gib-ON Design <gibonwroc@gmail.com>
2025-01-02 21:43:34 +0100

    esg part 2

--- src/app/[lang]/esg/page.jsx
+++ src/app/[lang]/esg/page.jsx
@@ -1,21 +1,21 @@
-import './esg.css'
-import translations from '@/data/esgPage'
+import "./esg.css";
+import translations from "@/data/esgPage";
 
 export default function esgPage({ params }) {
-	const { lang } = params
+	const { lang } = params;
 
 	return (
 		<>
 			<section>
 				<div className="wrapper hero esg-hero">
 					<div className="hero-text">
-						<h1>{translations[lang]?.title || 'title!'}</h1>
+						<h1>{translations[lang]?.title || "title!"}</h1>
 					</div>
 				</div>
 			</section>
 			<section>
 				<div className="wrapper">
-					<h2>{translations[lang]?.section1.sectionTitle || 'title!'}</h2>
+					<h2>{translations[lang]?.section1.sectionTitle || "title!"}</h2>
 					<ul className="pros-list">
 						{translations[lang]?.section1.features.map((feature, idx) => (
 							<li className="feature" key={idx}>
@@ -30,7 +30,7 @@ export default function esgPage({ params }) {
 				<div className="wrapper">
 					<div className="energy-hub">
 						<div className="energy-hub-title round-box">
-							<h2>{translations[lang]?.section2.sectionTitle || 'title!'}</h2>
+							<h2>{translations[lang]?.section2.sectionTitle || "title!"}</h2>
 						</div>
 						<ul className="energy-hub-features round-box">
 							{translations[lang]?.section2.features.map((feature, idx) => (
@@ -43,11 +43,27 @@ export default function esgPage({ params }) {
 					</div>
 				</div>
 			</section>
+			<section>
+				<div className="wrapper">
+					<h2>{translations[lang]?.section3.sectionTitle || "title!"}</h2>
+					<div className="esg-features">
+						{translations[lang]?.section3.features.map((feature, idx) => (
+							<div className="esg-feature" key={idx}>
+								<div className="esg-feature-icon">
+									<img src={feature.icon} alt={feature.featureName} />
+								</div>
+								<h3>{feature.featureName}</h3>
+								<p>{feature.featureDescription}</p>
+							</div>
+						))}
+					</div>
+				</div>
+			</section>
 		</>
-	)
+	);
 }
 
 export const generateStaticParams = async () => {
-	const langs = ['en', 'pl']
-	return langs.map(lang => ({ lang }))
-}
+	const langs = ["en", "pl"];
+	return langs.map((lang) => ({ lang }));
+};
--- src/data/esgPage.js
+++ src/data/esgPage.js
@@ -20,82 +20,163 @@ const translations = {
 				'Industrial and Production Facilities (transportation, metal, mining, agriculture, paper, plastic)',
 			]
 		},
+		section3: {
+			sectionTitle: 'Features',
+			features: [
+				{
+				icon: '/esg/icons/machine-learning.svg',
+				featureName: 'Machine learning',
+				featureDescription: 'Using advanced proprietary machine learning algorithms of anomaly detection and energy efficiency scoring and forecasting, the system can identify anomalies and patterns, empowering energy managers’ decision making.',
+			},
+				{
+				icon: '/esg/icons/rules-engine.svg',
+				featureName: 'Advanced Rules Engine',
+				featureDescription: 'Using a wizard-based interface, rules can be intuitively defined to activate or adjust field devices, settings, sensors, interface with third-party servers, send SMS/email notifications, and much more.',
+			},
+				{
+				icon: '/esg/icons/analytics.svg',
+				featureName: 'Analytics',
+				featureDescription: 'Dashboard-based analytics, visualizes empowering insights about energy consumption, patterns, and other operational parameters, on the fly or per user request, including sites’ comparison, normalized by square meters, and more.',
+			},
+				{
+				icon: '/esg/icons/smart-queries.svg',
+				featureName: 'B.I. - Smart Queries',
+				featureDescription: 'Leveraging big data and wide applicative knowhow, the system provides unique business intelligence (BI) insights, simplifying queries and data analytics, benchmarking and creating powerful managerial reports.',
+			},
+				{
+				icon: '/esg/icons/scheduler.svg',
+				featureName: 'Scheduler',
+				featureDescription: 'The system allows intuitive settings of repetitive tasks and sequences using the scheduler feature, optimizing energy usage and operational excellence for applicable sites.',
+			},
+				{
+				icon: '/esg/icons/reports.svg',
+				featureName: 'Periodic Energy Reports',
+				featureDescription: 'The system allows the creation of customized periodical reports per site or per multi-site grouping. Interactive reports enable a quick snapshot of energy parameters, analysis, benchmarking, and more.',
+			},
+				{
+				icon: '/esg/icons/energy-savings.svg',
+				featureName: 'Energy Saving Measurement Algorithms',
+				featureDescription: 'Empowered by advanced algorithms, the system monitors the energy usage and saving for each site or for a group of sites; available with sub-metering modules.',
+			},
+				{
+				icon: '/esg/icons/alerts.svg',
+				featureName: 'Alerts',
+				featureDescription: 'The system allows for highly flexible settings of alerts on energy and other operational parameters thresholds, fully customized by administrators. Alerts are displayed per site, or grouping of sites, and can be filtered as needed.',
+			},
+				{
+				icon: '/esg/icons/quality-scan.svg',
+				featureName: 'Power Quality Scan',
+				featureDescription: 'The system employs advanced upper harmonics scanning on main or on sub-metering channels in conjunction with its other electrical parameters measurement to provide Power Quality Analysis for sites.',
+			},
+				{
+				icon: '/esg/icons/iot-platforms.svg',
+				featureName: 'IoT Platform',
+				featureDescription: 'Advanced IoT platform integrating energy and multiple on-site sensors and actuators to effectively manage and control single to multi-site scale deployments.',
+			},
+				{
+				icon: '/esg/icons/safety-status.svg',
+				featureName: 'Safety Status Detection',
+				featureDescription: 'Extending the Power Quality Scan feature, the system is able to identify potential electrical safety hazards in a site’s infrastructure, creating a status report and alerts for managers to take preventive actions.',
+			},
+				{
+				icon: '/esg/icons/monitoring.svg',
+				featureName: 'Real Time Monitoring Dashboards',
+				featureDescription: 'Highly interactive dashboard monitoring screens displaying energy consumption, energy-saving, electrical figures, and site operational parameters – can be viewed by site or as multiple sites.',
+			},
+		
+		]
+		},
 	},
 	pl: {
-		title: "Referencje",
-		references: [
+		title: "ESG",
+		section1: {
+		  sectionTitle: 'Wiodący na rynku system oszczędzania i kontroli energii',
+		  features: [
+			'Całkowite oszczędności energii 15–20%',
+			'Okres zwrotu inwestycji 1,5–3 lata',
+			'Zintegrowane rozwiązanie: oszczędzanie energii, podliczniki, kontrola obciążenia, czujniki',
+			'Analiza danych',
+			'Uczenie maszynowe: wykrywanie anomalii, efektywność energetyczna, scoring',
+			'Zaawansowany silnik reguł',
+			'Potężna platforma oprogramowania oparta na chmurze IoT',
+			'Zaawansowana kontrola mocy i napięcia',
+		  ]
+		},
+		section2: {
+		  sectionTitle: 'Zastosowania centrów energetycznych',
+		  features: [
+			'Obiekty komercyjne (fast foody, restauracje, sklepy spożywcze, supermarkety, hotele, stacje benzynowe)',
+			'Sektor publiczny (szpitale, gminy, służby porządkowe, więzienia)',
+			'Obiekty przemysłowe i produkcyjne (transport, metalurgia, górnictwo, rolnictwo, papier, tworzywa sztuczne)',
+		  ]
+		},
+		section3: {
+		  sectionTitle: 'Funkcje',
+		  features: [
 			{
-				logo: "/references/echarris.png",
-				companyName: "tarczyński",
-				description:
-					"Z pełną odpowiedzialnością polecam Pana Dariusza Głowackiego jako menedżera zdolnego zarządzać skomplikowanymi inwestycjami od strony kosztowej i czasowej.",
-				pdfFile: "/documents/echarris-tarczynski.pdf",
+			  icon: '/esg/icons/machine-learning.svg',
+			  featureName: 'Uczenie maszynowe',
+			  featureDescription: 'Dzięki zaawansowanym algorytmom uczenia maszynowego system wykrywa anomalie i wzorce, wspierając podejmowanie decyzji przez menedżerów energii.',
 			},
 			{
-				logo: "/references/panattoni.png",
-				companyName: "amazon fulfillement center wro1",
-				description:
-					"Inżynier Głowacki wykonał wszystkie czynności z należytą starannością i Firma Panattoni Europe może polecić innym inwestorom inż. Głowackiego jako rzetelnego i godnego zaufania Inspektora Nadzoru Robót Budowlanych.",
-				pdfFile: "/documents/panattoni.pdf",
+			  icon: '/esg/icons/rules-engine.svg',
+			  featureName: 'Zaawansowany silnik reguł',
+			  featureDescription: 'Za pomocą interfejsu opartego na kreatorze można intuicyjnie definiować reguły aktywujące urządzenia, ustawienia, czujniki, a także wysyłające powiadomienia SMS/e-mail i wiele więcej.',
 			},
 			{
-				logo: "/references/eyemaxx.png",
-				companyName: "retail park oława",
-				description:
-					"Z pełną odpowiedzialnością polecamy zespół firmy PVM INWESTYCJE jako najwyższej próby menedżerów zdolnych zarządzać skomplikowanymi od strony technicznej, kosztowej i czasowej zadaniami inwestycyjnymi",
-				pdfFile: "/documents/eyemaxx-referencja-1.pdf",
+			  icon: '/esg/icons/analytics.svg',
+			  featureName: 'Analiza',
+			  featureDescription: 'Analizy wizualizowane na dashboardzie dostarczają kluczowych informacji o zużyciu energii, wzorcach i innych parametrach operacyjnych w czasie rzeczywistym lub na żądanie użytkownika.',
 			},
 			{
-				logo: "/references/fundacja.png",
-				companyName: "przylądek nadziei",
-				description:
-					"Fundacja “Na ratunek dzieciom z chorobą nowotworową” zaświadcza, że mgr inż. Dariusz Głowacki pełnił funkcję Kierownika Projektu przy budowie “Przylądka Nadziei” - rozbudowy Centrum Klinicznego Uniwersytetu Medycznego we Wrocławiu.",
-				pdfFile: "/documents/.pdf",
+			  icon: '/esg/icons/smart-queries.svg',
+			  featureName: 'Business Intelligence - Inteligentne zapytania',
+			  featureDescription: 'Dzięki wykorzystaniu big data i szerokiej wiedzy aplikacyjnej system dostarcza unikalne informacje Business Intelligence (BI), upraszczając zapytania, analizy danych i tworzenie raportów zarządczych.',
 			},
 			{
-				logo: "/references/aecom.png",
-				companyName: "bolesławiec city center",
-				description:
-					"Firma PVM INWESTYCJE świadczyła usługi z pełnym profesjonalizmem, zgodnie z wymogami Zamawiającego. Stwierdzam, że zamówienie zostało wykonanie należycie i terminowo.",
-				pdfFile: "/documents/aecom.pdf",
+			  icon: '/esg/icons/scheduler.svg',
+			  featureName: 'Harmonogram',
+			  featureDescription: 'System umożliwia intuicyjne ustawianie powtarzalnych zadań i sekwencji, optymalizując zużycie energii i doskonałość operacyjną.',
 			},
 			{
-				logo: "/references/echarris.png",
-				companyName: "firestone",
-				description:
-					"Z pełną odpowiedzialnością polecam Pana Dariusza Głowackiego jako menedżera zdolnego zarządzać skomplikowanymi inwestycjami od strony kosztowej i czasowej.",
-				pdfFile: "/documents/echarris-firestone.pdf",
+			  icon: '/esg/icons/reports.svg',
+			  featureName: 'Okresowe raporty energetyczne',
+			  featureDescription: 'System umożliwia tworzenie spersonalizowanych raportów okresowych dla pojedynczych lokalizacji lub grup lokalizacji, dostarczając szybki wgląd w parametry energetyczne, analizy i porównania.',
 			},
 			{
-				logo: "/references/bovis.png",
-				companyName: "galeria malta poznań & fabryka",
-				description:
-					"Z pełną odpowiedzialnością polecam Pan Dariusza Głowackiego jako najwyższej próby menadżera zdolnego zarządzać inwestycjami od strony technicznej, kosztowej i czasowej.",
-				pdfFile: "/documents/bovis.pdf",
+			  icon: '/esg/icons/energy-savings.svg',
+			  featureName: 'Algorytmy pomiaru oszczędności energii',
+			  featureDescription: 'Zaawansowane algorytmy monitorują zużycie energii i oszczędności dla każdej lokalizacji lub grupy lokalizacji, dostępne z modułami podlicznikowymi.',
 			},
 			{
-				logo: "/references/eyemaxx.png",
-				companyName: "retail park namysłów",
-				description:
-					"Z pełną odpowiedzialnością polecamy zespół firmy PVM INWESTYCJE jako najwyższej próby menedżerów zdolnych zarządzać skomplikowanymi od strony technicznej, kosztowej i czasowej zadaniami inwestycyjnymi.",
-				pdfFile: "/documents/eyemaxx-referencja-1.pdf",
+			  icon: '/esg/icons/alerts.svg',
+			  featureName: 'Alerty',
+			  featureDescription: 'System umożliwia elastyczne ustawienia alertów dla parametrów energetycznych i operacyjnych, dostosowane do potrzeb administratorów.',
 			},
 			{
-				logo: "/references/savant.png",
-				companyName: "hilton hotel & galeria bursztynowa",
-				description:
-					"I would not hesitate to recommend services of Mr. Dariusz Glowacki (PVM INWESTYCJE) as the supreme quality manager able to manage the investment from the technical, cost and time point of view.",
-				pdfFile: "/documents/savant.pdf",
+			  icon: '/esg/icons/quality-scan.svg',
+			  featureName: 'Skan jakości energii',
+			  featureDescription: 'Zaawansowane skanowanie harmoniczne oraz analiza parametrów elektrycznych zapewniają raporty o jakości energii dla lokalizacji.',
 			},
 			{
-				logo: "/references/michaliktomczak.png",
-				companyName: "hala logistyczna i magazyn blum Polska",
-				description:
-					"Z pełną odpowiedzialnością polecamy zespół Pana Dariusza Głowackiego jako najwyższej próby menedżerów zdolnych zarządzać skomplikowanymi od strony technicznej, kosztowej i czasowej zadaniami inwestycyjnymi.",
-				pdfFile: "/documents/mt.pdf",
+			  icon: '/esg/icons/iot-platforms.svg',
+			  featureName: 'Platforma IoT',
+			  featureDescription: 'Zaawansowana platforma IoT integrująca energię i różnorodne czujniki na miejscu, zarządzająca od pojedynczych do wielostanowiskowych wdrożeń.',
 			},
-		],
-	},
+			{
+			  icon: '/esg/icons/safety-status.svg',
+			  featureName: 'Wykrywanie statusu bezpieczeństwa',
+			  featureDescription: 'System identyfikuje potencjalne zagrożenia bezpieczeństwa elektrycznego w infrastrukturze lokalizacji, generując raporty i alerty.',
+			},
+			{
+			  icon: '/esg/icons/monitoring.svg',
+			  featureName: 'Pulpity monitorowania w czasie rzeczywistym',
+			  featureDescription: 'Interaktywne pulpity wyświetlają zużycie energii, oszczędności, dane elektryczne i operacyjne – dla jednej lokalizacji lub wielu lokalizacji jednocześnie.',
+			},
+		  ]
+		},
+	  }
+	  
 };
 
 export default translations;
